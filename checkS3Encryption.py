0b2a75f73ea5c33832cbb42fef8212902c2b38d0
sp0f <adam.lyjak@gmail.com>
2017-12-14 13:53:07 +0100

    +exclude list

--- checkS3Encryption.py
+++ checkS3Encryption.py
@@ -4,12 +4,27 @@
 import boto3
 import botocore
 from sys import exit
+import os.path
 
 s3 = boto3.resource('s3')
 s3client = boto3.client('s3')
 
 buckets = s3.buckets.all()
 unencrypted_bucket_list= []
+
+exclude_list=[]
+exclude_file="s3Encryption-exclude_list.txt"
+
+if os.path.isfile(exclude_file):
+    with  open(exclude_file, 'r') as f:
+        while True:
+            line=f.readline()
+            if not line:
+                break
+            else:
+                exclude_list.append(line.rstrip())
+
+# print(exclude_list)
 
 for bucket in buckets:
     try:
@@ -19,7 +34,8 @@ for bucket in buckets:
     except botocore.exceptions.ClientError as err:
         if err.response['Error']['Code'] == "ServerSideEncryptionConfigurationNotFoundError":
             # print(bucket.name)
-            unencrypted_bucket_list.append(bucket.name)
+            if bucket.name not in exclude_list:
+                unencrypted_bucket_list.append(bucket.name)
         else:
             raise err
 
