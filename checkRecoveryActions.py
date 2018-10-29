3243a1d132f79c5fff129da8274ffa1ae87d6227
Adam Łyjak <adam.lyjak@gmail.com>
2018-10-29 13:28:21 +0100

    - tag checking, moved to different check

--- checkRecoveryActions.py
+++ checkRecoveryActions.py
@@ -1,51 +1,30 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import boto3
 from sys import exit
 
-"""
-Search for instances without 'autorecovery' tag and those with tag but without proper alert action.
+"""Search for instances without configured auto recovery action."""
 
-LIMITATION: boto3 can featch only 100 alerts without filters. If you have more the 100 please add some sort
-of filtering to cloudwatch.describe_alarms()
+aws_region = 'eu-west-1'
+# aws_profile = 'default'
 
-BLAME: sp0f@sigterm.pl
-"""
-
-ec2 = boto3.resource('ec2')
-cloudwatch = boto3.client('cloudwatch')
-
-def findInstancesWithoutTag(tagName):
-    """"Find instances without tag"""
-    instances = ec2.instances.all()
-
-    instancesList = []  # list of instances without backup tag
-    for instance in instances:
-        # print(instance.id)
-        tagFound = False
-        try:
-            for tag in instance.tags:
-                if tag['Key'] == tagName:
-                    tagFound = True
-        except NoneType:
-            instanceList.append(instance.id)
-            continue
-        if tagFound == False:
-            instancesList.append(instance.id)
-    return instancesList
+session = boto3.Session(region_name=aws_region)
+ec2 = session.resource('ec2')
+cloudwatch = session.client('cloudwatch')
 
 def getInstancesWithTagValue(tagName,tagValue="true"):
-    """Find instances that have tag `tagName` set to value `tagValue` (default: true)"""
+    """Find instances that have tag `tagName` set to value `tagValue` (default: true)."""
 
     instances = ec2.instances.filter(Filters=[{"Name": "tag:"+tagName, "Values": [tagValue]}])
     instancesId = []
     for instance in instances:
-        #instanceName=getTag(instance,"Name")
         instancesId.append(instance.id)
     return instancesId
 
+
 def getTag(taggedObject, tagKey):
-    """get tag defined by tagKey param for collection(ec2.Instance, ec2.Image etc.)"""
+    """Get tag defined by tagKey param for collection(ec2.Instance, ec2.Image etc.)."""
     for tag in taggedObject.tags:
         if tag['Key'] == tagKey:
             return tag['Value']
@@ -70,15 +49,10 @@ def main():
             break
 
     taggedInstances = getInstancesWithTagValue("autorecovery","true")
-    untaggedInstances = findInstancesWithoutTag("autorecovery")
     alarmingInstances = set(taggedInstances)- set(recoveryEnabledInstances)
 
     # Nagios check output
-
     criticalMessage="CRITICAL"
-    # alert about instances without `recovery` tag
-    if len(untaggedInstances) != 0:
-        criticalMessage=criticalMessage+" instances without autorecovery tag: "+" ".join(untaggedInstances)
 
     # alert about instances with `recovery` tag set to `true` without recovery action
     if len(alarmingInstances) != 0:
@@ -86,10 +60,10 @@ def main():
 
     # return check message and status
     if criticalMessage != "CRITICAL":
-        print criticalMessage
+        print(criticalMessage)
         exit(2)
     else:
-        print "OK"
+        print("OK")
         exit(0)
 
 
