9552f5a242d29bc47a0cb55f079dd81e07a9fa95
Adam Łyjak <adam.lyjak@sigterm.pl>
2019-12-16 11:51:16 +0100

    expireDate

--- snapshotMonitor.py
+++ snapshotMonitor.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import boto3
+import re
 from sys import exit
+from datetime import datetime, timedelta
 
 owner_id = '260187409195'
 ec2 = boto3.client('ec2')
+max_age = 180 # days
 
 def all_ami_snapshots():
     """Return list of all snapshots ids related to AMI"""
@@ -62,6 +66,31 @@ def find_untaged_snapshots(tagKey,snapshot_ids):
             untagged_snapshots.append(snap['SnapshotId'])
     return untagged_snapshots
 
+def verify_expire_tag(snapshot_ids, tag_key):
+    expired_snapshots=[]
+    snapshots = ec2.describe_snapshots(SnapshotIds=snapshot_ids)['Snapshots']
+    expire_date_format = re.compile("\d\d-\d\d-\d\d\d\d")
+
+    for snap in snapshots:
+        no_tag = True
+        if 'Tags' in snap:
+            for tag in snap['Tags']:
+                if tag['Key'] == tag_key:
+                    if (not expire_date_format.fullmatch(tag['Value'])):
+                        expire_date_format.append(snap['SnapshotId'])
+                    expire_datetime = datetime.strptime(tag['Value'], "%d-%m-%Y")
+                    if (expire_datetime <= datetime.today()):
+                        expired_snapshots.append(snap['SnapshotId'])
+                    if (expire_datetime >= (datetime.today() + timedelta(days=max_age))):
+                        expired_snapshots.append((snap['SnapshotId']))
+                    else:
+                        no_tag = False
+        if no_tag:
+            expired_snapshots.append(snap['SnapshotId'])
+
+    return expired_snapshots
+
+
 def find_unencrypted_snapshots(snapshot_ids):
     unencrypted_snapshots=[]
     snapshots = ec2.describe_snapshots(SnapshotIds=snapshot_ids)['Snapshots']
@@ -97,7 +126,8 @@ def main():
 
     # dirty magic, dirty tricks
     non_ami_snapshots = list(set(all_snapshots_ids)-set(ami_snapshots_ids))
-    untagged_snapshots = find_untaged_snapshots('archive',non_ami_snapshots)
+    # untagged_snapshots = find_untaged_snapshots('archive',non_ami_snapshots)
+    expired_snapshots = verify_expire_tag(non_ami_snapshots,'expireDate')
     unencrypted_snapshots = find_unencrypted_snapshots(non_ami_snapshots)
 
 
@@ -108,8 +138,8 @@ def main():
         criticalMessage = criticalMessage + " abandoned AMI snapshots: " + " ".join(abandoned_snapshots)
 
     # alert about untaged snapshots (non ami snapshots)
-    if len(untagged_snapshots) != 0:
-        criticalMessage = criticalMessage + " untagged snapshots: " + " ".join(untagged_snapshots)
+    if len(expired_snapshots) != 0:
+        criticalMessage = criticalMessage + " expired (expireDate < now()+"+str(max_age)+" or wrongly tagged snapshots: " + " ".join(expired_snapshots)
 
     if len(unencrypted_snapshots) != 0:
         criticalMessage = criticalMessage + " unencrypted snapshots: " + " ".join(unencrypted_snapshots)
